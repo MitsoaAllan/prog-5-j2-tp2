7496f8384d27efe0280c1d23612030689f94f92b
MitsoaAllan <hei.mitsoa@gmail.com>
2025-11-30 21:50:47 +0300

    feat: Initial commit

---
+++ src/test/java/com/app/rent/Prog5J2Tp2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.rent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Prog5J2Tp2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
