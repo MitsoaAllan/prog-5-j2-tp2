8da10adef00033d5ed1a5840547d6723531fef65
MitsoaAllan <hei.mitsoa@gmail.com>
2025-12-01 01:53:23 +0300

    test: backing to the latest commit where CI passes

--- src/main/java/com/app/rent/model/rent/Renter.java
+++ src/main/java/com/app/rent/model/rent/Renter.java
@@ -9,21 +9,21 @@ import java.util.Set;
 
 public abstract class Renter implements IRent{
     protected int id;
-    protected String addréss;
+    protected String address;
     protected String phone;
     protected String email;
     protected Set<Property> properties;
 
     public Renter(int id, String address, String phone, String email) {
         this.id = id;
-        this.addréss = address;
+        this.address = address;
         this.phone = phone;
         this.email = email;
         this.properties = new HashSet<>();
     }
 
     public int getId() { return id; }
-    public String getAddréss() { return addréss; }
+    public String getAddress() { return address; }
     public String getPhone() { return phone; }
     public String getEmail() { return email; }
     public Set<Property> getProperties() { return properties; }
